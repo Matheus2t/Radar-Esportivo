a9d3f9c9cfbdea6db82ffde7bc32d6540d0a9cd5
Matheus2T <matheusteteuu07@gmail.com>
2026-05-21 17:38:16 -0300

    feat: adicionar lista inicial dos jogos

---
+++ jogos.py
@@ -0,0 +1,24 @@
+jogos = [
+    {
+        "casa": "Arsenal",
+        "fora": "Chelsea",
+        "liga": "Premier League",
+        "odd_casa": 1.75,
+        "status": "pré-live",
+    },
+    {
+        "casa": "Palmeiras",
+        "fora": "Flamengo",
+        "liga": "Brasileirão",
+        "odd_casa": 1.60,
+        "status": "pré-live",
+    },
+    {
+        "casa": "Real Madrid",
+        "fora": "Barcelona",
+        "liga": "La liga",
+        "odd_casa": 2.10,
+        "status": "ao vivo",
+    },
+
+]
