2b40f238766e9779f6924c7bedc378bcf6cb419f
Matheus2T <matheusteteuu07@gmail.com>
2026-05-24 23:24:24 -0300

    feat ignorar maiusculas nos filtros

--- backend/radar.py
+++ backend/radar.py
@@ -4,7 +4,7 @@ def verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido):
     alertas = []
 
     for jogo in jogos:
-        if jogo["odd_casa"] <= valor_odd and jogo["liga"] == liga_escolhida and jogo["status"] == status_escolhido:            
+        if jogo["odd_casa"] <= valor_odd and jogo["liga"].lower() == liga_escolhida.lower() and jogo["status"].lower() == status_escolhido.lower():            
             alertas.append(jogo)
             
     return alertas  
