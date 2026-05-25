669c99b46764f701f11582b120eebc103d554977
Matheus2T <matheusteteuu07@gmail.com>
2026-05-24 23:19:47 -0300

    feat: adicionar filtro por status

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,9 @@ from radar import verificar_odds
 
 valor_odd = float(input("Qual o valor da odd que você quer: "))
 liga_escolhida = input("Qual liga deseja escolher: ")
+status_escolhido = input("Qual status deseja filtrar: ")
 
-alertas = verificar_odds(jogos, valor_odd, liga_escolhida)
+alertas = verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido)
 
 if len(alertas) == 0:
     print("Não foi encontrado nenhum jogo em nossa base de dados")
--- backend/radar.py
+++ backend/radar.py
@@ -1,18 +1,19 @@
 from jogos import jogos
 
-def verificar_odds(jogos, valor_odd, liga_escolhida):
+def verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido):
     alertas = []
 
     for jogo in jogos:
-        if jogo["odd_casa"] <= valor_odd and jogo["liga"] == liga_escolhida:            
+        if jogo["odd_casa"] <= valor_odd and jogo["liga"] == liga_escolhida and jogo["status"] == status_escolhido:            
             alertas.append(jogo)
             
     return alertas  
 
 valor_odd = float(input("Qual o valor da odd que você quer: "))
 liga_escolhida = input("Qual liga deseja escolher: ")
+status_escolhido = input("Qual status deseja filtrar: ")
 
-alertas = verificar_odds(jogos, valor_odd, liga_escolhida)
+alertas = verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido)
 
 if len(alertas) == 0:
     print("Não foi encontrado nenhum jogo em nossa base de dados")
