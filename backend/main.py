49f00df6a3e3f241c90bb8437ea02d012b645487
Matheus2T <matheusteteuu07@gmail.com>
2026-05-25 23:48:49 -0300

    style: melhorar exibicao dos alertas

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 from jogos import jogos
 from radar import verificar_odds
 
+
+
+
+valor_odd = float(input("Qual o valor da odd que você quer: "))
 print("Ligas disponíveis:")
 for jogo in jogos:
     print("-", jogo["liga"])
-
-valor_odd = float(input("Qual o valor da odd que você quer: "))
 liga_escolhida = input("Qual liga deseja escolher: ")
+print("Status disponíveis:")
+for jogo in jogos:
+    print("-", jogo["status"])
 status_escolhido = input("Qual status deseja filtrar: ")
 
 alertas = verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido)
