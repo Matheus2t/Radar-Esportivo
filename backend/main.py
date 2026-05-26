178f70ed8eadfa5d6a1819c216ab65458c5136c3
Matheus2T <matheusteteuu07@gmail.com>
2026-05-25 23:47:33 -0300

    feat: exibir ligas disponiveis

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from jogos import jogos
 from radar import verificar_odds
 
-valor_odd = float(input("Qual o valor da odd que você quer: "))
-liga_escolhida = input("Qual liga deseja escolher: ")
 print("Ligas disponíveis:")
 for jogo in jogos:
-    print(jogo["liga"])
+    print("-", jogo["liga"])
+
+valor_odd = float(input("Qual o valor da odd que você quer: "))
+liga_escolhida = input("Qual liga deseja escolher: ")
 status_escolhido = input("Qual status deseja filtrar: ")
 
 alertas = verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido)
