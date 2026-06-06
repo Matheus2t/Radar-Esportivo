1c63821c15983d1ec004f9d3485e7942ce0315ed
Matheus2T <matheusteteuu07@gmail.com>
2026-06-05 23:16:23 -0300

    feat: adicionar contagem de jogos por liga

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,28 @@
 from jogos import jogos
-from radar import verificar_odds, listar_ligas, listar_odd, listar_status
+from radar import verificar_odds, listar_ligas, listar_odd, listar_status, contar_jogos_por_liga
+
+print("jogos por liga:")
+contagem_ligas = contar_jogos_por_liga(jogos)
+
+for liga, quantidade in contagem_liga.items():
+    print("-", liga + ":", quantidade)
 
 print("Odds disponíveis:")
 for odd in listar_odd(jogos):
     print("-", odd)
+
 valor_odd = float(input("Qual o valor da odd que você quer: "))
+
 print("Ligas disponíveis:")
 for liga in listar_ligas(jogos):
     print("-", liga)
+
 liga_escolhida = input("Qual liga deseja escolher: ")
+
 print("Status disponíveis:")
 for status in listar_status(jogos):
     print("-", status)
+
 status_escolhido = input("Qual status deseja filtrar: ")
 
 alertas = verificar_odds(jogos, valor_odd, liga_escolhida, status_escolhido)
@@ -21,11 +32,18 @@ if len(alertas) == 0:
     print("Não foi encontrado nenhum jogo em nossa base de dados, que possui", len(jogos), "jogos")
 else:
     print("Foram encontrados", len(alertas) , "jogos")
+
     tipo_classificacao = input("Qual o tipo da classificação desejada? [geral], [casa], [fora], [todos]: ").lower()
+
     if tipo_classificacao not in ["geral", "casa", "fora", "todos"]:
         tipo_classificacao = "todos"
-    for alerta in alertas_ordenados:        
-        print("Radar:", alerta["casa"], "x", alerta["fora"], "-", alerta["odd_casa"])
+
+        for alerta in alertas_ordenados:
+            print("Radar:", alerta["casa"], "x", alerta["fora"])
+            print("Odd:", alerta["odd_casa"])
+            print("Liga:", alerta["liga"])
+            print("Status:", alerta["status"])
+
         if tipo_classificacao == "geral":
             print(alerta["casa"], "- Geral:", alerta["classificacao_casa"]["geral"])
             print(alerta["fora"], "- Geral:", alerta["classificacao_fora"]["geral"])
@@ -41,7 +59,8 @@ else:
             print(alerta["casa"], "- Casa:", alerta["classificacao_casa"]["casa"])
             print(alerta["fora"], "- Casa:", alerta["classificacao_fora"]["casa"])
             print(alerta["casa"], "- Fora:", alerta["classificacao_casa"]["fora"])
-            print(alerta["fora"], "- Fora:", alerta["classificacao_fora"]["fora"])          
+            print(alerta["fora"], "- Fora:", alerta["classificacao_fora"]["fora"])  
+                    
         if "h2h" in alerta:
             print("H2H:")
             for confronto in alerta["h2h"]:
